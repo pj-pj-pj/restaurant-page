443b09f3af35584fb04df45adf05e91dec415bb3
PJ <96671928+Peaksenut@users.noreply.github.com>
2023-05-31 20:12:52 +0800

    add page tracker

--- src/index.js
+++ src/index.js
@@ -3,3 +3,40 @@ import homepageInit from './homepage';
 
 document.querySelector("link[rel~='icon']").href = favicon;
 homepageInit();
+
+const currentPage = {
+  home: true,
+  menu: false,
+  contact: false,
+};
+
+const content = document.querySelector('div#content');
+const navbar = document.querySelector('.navbar');
+navbar.addEventListener('click', (e) => {
+  if (!currentPage.home && e.target.textContent === 'Home') {
+    clearContent();
+    currentPage.home = true;
+    currentPage.menu = false;
+    currentPage.contact = false;
+  }
+
+  if (!currentPage.menu && e.target.textContent === 'Menu') {
+    clearContent();
+    currentPage.home = false;
+    currentPage.menu = true;
+    currentPage.contact = false;
+  }
+
+  if (!currentPage.contact && e.target.textContent === 'Contact Us') {
+    clearContent();
+    currentPage.home = false;
+    currentPage.menu = false;
+    currentPage.contact = true;
+  }
+});
+
+function clearContent() {
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
+}
